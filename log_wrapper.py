233b1161f598197cbef48f7fbb16d6a5353425fb
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-08-29 10:54:09 +0100

    Implement logging class.

---
+++ log_wrapper.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+LOG_FORMAT = '%(asctime)s %(message)s'
+DEFAULT_LEVEL = logging.DEBUG
+
+class LogWrapper():
+
+    def __init__(self, name, mode="w", path=None):
+        self.path=path
+        self.create_directory()
+        self.filename = self.path + "/" + name + ".log"
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(DEFAULT_LEVEL)
+
+        file_handler = logging.FileHandler(self.filename, mode=mode)
+        formatter = logging.Formatter(LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        self.logger.info("LogWrapper init()" + self.filename)
+
+
+    def create_directory(self):
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+if __name__ == "__main__":
+    log = LogWrapper(name="Test", path="./logs")
+    log.logger.debug("Hello")
