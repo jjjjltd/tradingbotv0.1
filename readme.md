91fb86e24e0004ccf660d8580fe70e3366fa8524
jjjjltd <johnjoyce90@hotmail.co.uk>
2023-08-29 19:53:47 +0100

    Implement Timings class, draft 1, pre-V0.1

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Progress:  https://www.youtube.com/watch?v=I3KFCXgVrZ4&list=PLZ1QII7yudbecO6a-zAI6cuGP1LLnmW8e&index=70
+Progress:  https://www.youtube.com/watch?v=eEpKWXpcEus&list=PLZ1QII7yudbecO6a-zAI6cuGP1LLnmW8e&index=74  05:00
---
+++ timing.py
@@ -0,0 +1,8 @@
+from utils import time_utc
+import datetime as dt
+
+class Timing():
+    def __init__(self, last_candle):
+        self.last_candle = last_candle
+        if last_candle is None:
+            self.last_candle = time_utc() - dt.timedelta(minutes=10)
